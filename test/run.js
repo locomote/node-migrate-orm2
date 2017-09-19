738479983ae683c5745ce0ef07ce747a02f4a2d1
Yevheniy Miropolets <yevheniy.miropolets@gmail.com>
2017-09-19 12:48:32 +0300

    refactor run.js to run test from a few directories

--- test/run.js
+++ test/run.js
@@ -3,26 +3,29 @@ var fs      = require('fs');
 var path    = require('path');
 var helpers = require('./helpers');
 
+const Promise = require('bluebird');
+
 var mocha      = new Mocha({ reporter: "spec" });
-var location   = path.normalize(path.join(__dirname, 'integration'));
 var configPath = path.normalize(path.join(__dirname, 'config.js'));
+
 
 if (!helpers.isTravis() && !fs.existsSync(configPath)) {
   console.error("test/config.js is missing. Take a look at test/config.example.js");
   process.exit(1);
 }
 
-function runTests(cb) {
-  fs.readdirSync(location).filter(function (file) {
+function runTests(dir, cb) {
+  fs.readdirSync(dir).filter(function (file) {
     return file.substr(-3) === '.js';
   }).forEach(function (file) {
     mocha.addFile(
-      path.join(location, file)
+      path.join(dir, file)
     );
   });
 
   mocha.run(cb);
 }
+const runTestsAsync = Promise.promisify(runTests);
 
 var protocol = helpers.protocol();
 var protocols = [];
@@ -41,14 +44,18 @@ function run (err) {
   }
   if (!pr) return process.exit(0);
 
-  process.env.ORM_PROTOCOL = pr
+  process.env.ORM_PROTOCOL = pr;
 
   console.log(
     "\n\nRunning", pr, "tests",
     "\n------------------------"
   );
 
-  runTests(run);
+  Promise.mapSeries(['integration'], function(dir) {
+    return runTestsAsync(path.normalize(path.join(__dirname, dir)));
+  })
+    .then(() => run())
+    .catch(run);
 }
 
 run();
