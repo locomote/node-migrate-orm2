291c526147a9316711f5e2e75984a2f809d3e800
Serhii Ishchenko <sishchenko@locomote.com>
2017-09-20 22:07:49 +0300

    Improve explanation in migration examples.

--- examples/migrations/007-add-foreign-key-promise.js
+++ examples/migrations/007-add-foreign-key-promise.js
@@ -1,8 +1,8 @@
 // Note - this is an example only. Table2
-// You can use Promise instead of callback in both `up` and `down` methods
-// like in this example
-exports.up = function() { // do not declare `next` here
-  return this.addForeignKey('test_table2', // do not forget `return` here
+// You can return Promise from both `up` and `down` methods instead of using `next` callback.
+// Public methods of MigrationDSL return Promise, so you can use them like in the example below:
+exports.up = function() { // do not declare `next` argument here (and do not pass it into further calls)
+  return this.addForeignKey('test_table2', // do not forget to `return` the Promise
     { name:       'table1id',
       references: { table: 'table1', column: 'id' }
     });
